52d4af2c9f1b9d3229f29a3c384194c9564d21a1
sharath <csharath039@gmail.com>
2026-07-29 10:45:17 +0530

    added all plots

--- allplot.py
+++ allplot.py
@@ -1,9 +1,14 @@
+
+
+
+
+
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-d = np.read_csv('titanic.csv')
+d = pd.read_csv('titanic.csv')
 
 x=d["KM"]
 y=d["Weight"]
@@ -11,5 +16,66 @@ z=d["Price"]
 
 # 3d surface
 a = plt.axes(projection="3d")
-a.trisurf(x,y,z)
+a.plot_trisurf(x,y,z)
 plt.show()
+
+
+
+#2
+#Visualize the n-dimensional data using contour plots.
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+data = pd.read_csv("ToyotaCorolla.csv")
+
+#contour plot
+x = data['KM']
+y = data['Weight']
+z = data['Price']
+
+plt.tricontourf(x, y, z, levels=20, cmap='jet')
+plt.colorbar(label='Price')
+plt.xlabel('KM')
+plt.ylabel('Weight')
+plt.title('Contour Plot')
+plt.show()
+
+
+
+
+
+#3
+#Visualize the n-dimensional data using heat-map.
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+data = pd.read_csv("ToyotaCorolla.csv")
+
+#heat map
+sns.heatmap(data[["Price","KM","Doors", "Weight"]].corr(),cmap='jet',annot=True)
+plt.show()
+
+
+#4
+# Visualize the n-dimensional data using Box-plot.
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+data = pd.read_csv("ToyotaCorolla.csv")
+
+#box plot
+plt.title('Box Plot')
+plt.boxplot([data["Price"],data["HP"],data["KM"]])
+
+plt.xticks([1,2,3],["Price","HP","KM"])
+
+plt.show()
+
+
+
